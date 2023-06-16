726b8e22533d95979c0d4d9d8d25afe14e165581
Mark Chmarny <mark@chmarny.com>
2023-06-16 11:52:38 -0700

    update template

--- deployments/machine.tf
+++ deployments/machine.tf
@@ -1,6 +1,10 @@
 # VM template for runner
+resource "random_id" "id" {
+	  byte_length = 4
+}
+
 resource "google_compute_instance_template" "runner_template" {
-  name        = "${var.name}-template"
+  name        = "${var.name}-template-${random_id.id.hex}"
   machine_type = var.machine
   region       = var.region
 
@@ -12,7 +16,7 @@ resource "google_compute_instance_template" "runner_template" {
 
   # TODO: Parameterize the disk
   disk {
-    disk_type    = "pd-standard"
+    disk_type    = "pd-ssd"
     source_image = "ubuntu-2204-jammy-v20230616"
     disk_size_gb = 10
     boot         = true
@@ -20,7 +24,10 @@ resource "google_compute_instance_template" "runner_template" {
   }
 
   network_interface {
-    network = "default"
+    network = data.google_compute_network.default.self_link
+    access_config {
+      // Ephemeral IP
+    }
   }
 
   service_account {
@@ -29,8 +36,8 @@ resource "google_compute_instance_template" "runner_template" {
   }
 
   metadata = {
-    startup_script          = "${file("../scripts/startup")}"
-    shutdown-script         = "${file("../scripts/shutdown")}"
+    startup-script          = file("../scripts/startup")
+    shutdown-script         = file("../scripts/shutdown")
     enable-guest-attributes = "true"
     enable-osconfig         = "true"
   }
@@ -48,7 +55,6 @@ resource "google_compute_instance_template" "runner_template" {
   }
 }
 
-// Rundom name to force rolling updates when something changes in the template
 resource "google_compute_region_instance_group_manager" "mig" {
   name               = "${var.name}-mig"
   base_instance_name = var.name
