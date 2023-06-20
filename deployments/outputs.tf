60bdd8379a0cbf508a014a0dca48651d5cf665cc
Mark Chmarny <mark@chmarny.com>
2023-06-20 13:24:49 -0700

    add slsa attestation test

--- deployments/outputs.tf
+++ deployments/outputs.tf
@@ -14,3 +14,13 @@ output "REGISTRY_URI" {
   value       = "${google_artifact_registry_repository.registry.location}-docker.pkg.dev/${data.google_project.project.name}/${google_artifact_registry_repository.registry.name}"
   description = "Artifact Registry URI."
 }
+
+output "CI_SERVICE_ACCOUNT" {
+  value       = google_service_account.ci_sa.email
+  description = "Service account to use in GitHub Action for federated auth."
+}
+
+output "CI_IDENTITY_PROVIDER" {
+  value       = google_iam_workload_identity_pool_provider.github_provider.name
+  description = "Provider ID to use in Auth action for GCP in GitHub."
+}
